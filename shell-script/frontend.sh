1bb1b0e3ad2e4722856ec9d422b14110b701ffff
sivaram <siva.ram.konka@gmail.com>
2024-07-05 13:57:46 +0000

    little changes

--- shell-script/frontend.sh
+++ shell-script/frontend.sh
@@ -26,34 +26,14 @@ else
 fi
 
 cat <<EOF > /etc/nginx/default.d/expense.conf
-server {
-    listen 80 default_server;
-    server_name localhost;
+proxy_http_version 1.1;
 
-    root /usr/share/nginx/html;
-    index index.html;
+location /api/ { proxy_pass http://localhost:8080/; }
 
-    location / {
-        try_files  / =404;
-    }
-
-    location /api/ {
-        proxy_pass http://expense.backend.test.ullagallu.cloud:8080/;
-        proxy_http_version 1.1;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-    }
-
-    location /health {
-        stub_status on;
-        access_log off;
-    }
-
-    # Additional location blocks or configurations can go here
+location /health {
+  stub_status on;
+  access_log off;
 }
-
 EOF
 VALIDATE $? "Creating Nginx reverse proxy configuration"
 
